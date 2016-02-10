8113d0f7ee9231d94fc2fb9c5321797a302ceecf
Thien Vo <vohuythien@utexas.edu>
2016-02-10 01:01:53 -0500

    Removed author's link from !help

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -275,7 +275,6 @@ class MusicBot(discord.Client):
 
         helpmsg += ", ".join(commands)
         helpmsg += "```"
-        helpmsg += "https://github.com/SexualRhinoceros/MusicBot/wiki/Commands-list"
 
         return Response(helpmsg, reply=True, delete_after=60)
 
